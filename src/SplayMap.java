1a3ec98c3ebae2c789e36eaa5b467d652c027305
Jraange <jraange@gmail.com>
2013-02-26 00:27:16 +0500

    First commit

---
+++ src/SplayMap.java
@@ -0,0 +1,108 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jrange
+ * Date: 26.02.13
+ * Time: 0:22
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class SplayMap<K extends Comparable<K>, V> implements Map<K, V> {
+
+    private Node root;
+
+    private class Node {
+        K key;
+        V value;
+        Node left;
+        Node right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+
+    }
+    public Node splay(Node h, K key)
+    {
+        return null;
+    }
+
+
+    @Override
+    public void clear() {
+        root = null;
+        // TODO Auto-generated method stub
+
+    }
+
+    public boolean containsKey(Object arg0) {
+        K key = (K) arg0;
+        return (get(key) != null);
+    }
+
+    @Override
+    public boolean containsValue(Object arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Set<java.util.Map.Entry<K, V>> entrySet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public V get(Object arg0) {
+        K key = (K) arg0;
+        root = splay(root, key);
+        int cmp = (key).compareTo(root.key);
+        if (cmp == 0) return root.value;
+        else          return null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public V put(K arg0, V arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public V remove(Object arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Collection<V> values() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
